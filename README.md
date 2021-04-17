b0bd2a4f863b727092bdb23117fc44c60914d8bf
Adam Spiegel <adamspiegel23@gmail.com>
2021-04-17 17:26:41 -0400

    update repository

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Code-Quiz
+# **JavaScript Code-Quiz**
+
+## Project Description & Goals
+   The goal of this project was to create and develop a functioning code quiz on Javascript.  The code quiz was to include a timer, multiple choice questions and a high score storage function as well.  The user was to be able to run through a series of questions, see their score, and then log their initials.  
+
+
+## Action Items 
+
+    1  Initially I developed my html, css and javascript files which had to be created and saved to develop the coding quiz upon.
+
+    2  Next, my html code was developed to represent all components within my head, body, main and individual sections.  Of which, button classes, div ids and div classes were developed to function with javascript.
+
+    3  Next, I began to develop my javascript which inlcuded all project variables intially.
+
+    4  All quiz questions themselves were then developed and listed within the larger array, quizQuestions.
+
+    5  An addEventlister was then created to develop functions for my timer and start quiz button.
+
+    6  Console logs were created to track the funtionality of the timer and quiz questions being deployed.
+
+    7  Next I created and developed the function runQuestions to deploy the questions to the user for answers.
+
+    8 Finally, I utilized my css file to style the page accordingly so it was visually more appealing to the user.
+
+    * Currently in Progress Due to Troubleshooting/Confusion - 
+    *Items which I am currently having great difficulty with deploying and are still in development include:
+      - progressing from question 1 onto the questions 2-5 appropriatley.
+      - adding in radio buttons
+      - generating the answers to display for the user
+      - logging the users final score and initials
+
+
+
+ <img src="JS Code Quiz.png" alt="JS Code Quiz Image">
+
+
+
+## Live Link to Deployed Page
+
+ https://adamspiegel.github.io/Code-Quiz/
+
+ 
+# Contact Information
+* Email - AdamSpiegel23@gmail.com
+* GitHub - https://github.com/AdamSpiegel
+* LinkedIn - www.linkedin.com/in/adam-spiegel-3086687
+
--- script.js
+++ script.js
@@ -1,55 +1,116 @@
+// All functional variables for JS code quiz
 var startButton = document.querySelector (".start-button"); 
 var highScores = document.querySelector (".high-scores");
 var quizQuestions = document.querySelector (".quiz-questions");
 var gameTimer = document.querySelector (".game-timer");
-var gameOver = document.querySelector (".game-over")
+var gameOver = document.querySelector (".game-over");
+var multiChoice = document.querySelector (".multiple-choice");
+var highScores = document.querySelector (".high-score"); 
+var currentQuestion = document.querySelector ("#current-question");
+var playAgain = document.querySelector ("play-again")
 
-var quizQuestions = [{
-    question: "How many elements can you apply an 'ID' attribute to?",
-    choiceA: "As many as you want",
-    choiceB: "3",
-    choiceC: "1",
-    choiceD: "128",
-    correctAnswer: "c"},
-  {
-    question: "What does DOM stand for?",
-    choiceA: "Document Object Model",
-    choiceB: "Display Object Management",
-    choiceC: "Digital Ordinance Model",
-    choiceD: "Desktop Oriented Mode",
-    correctAnswer: "a"},
-   {
-    question: "What is used primarily to add styling to a web page?",
-    choiceA: "HTML",
-    choiceB: "CSS",
-    choiceC: "Python",
-    choiceD: "React.js",
-    correctAnswer: "b"},
+var playerInitals = []
+var count = 60; 
+
+var qIndex = 0
+
+// var choiceA = ""
+// var choiceB = ""
+// var choiceC = ""
+// var choiceD = ""
+
+// 5 quiz questions to be run to test user
+  var quizQuestions =[
     {
-    question: "What HTML tags are JavaScript code wrapped in?",
-    choiceA: "&lt;div&gt;",
-    choiceB: "&lt;link&gt;",
-    choiceC: "&lt;head&gt;",
-    choiceD: "&lt;script&gt;",
-    correctAnswer: "d"},
-    {
-    question: "When is localStorage data cleared?",
-    choiceA: "No expiration time",
-    choiceB: "On page reload",
-    choiceC: "On browser close",
-    choiceD: "On computer restart",
-    correctAnswer: "a"},  
-    {
-    question: "What does WWW stand for?",
-    choiceA: "Web World Workings",
-    choiceB: "Weak Winter Wind",
-    choiceC: "World Wide Web",
-    choiceD: "Wendy Wants Waffles",
-    correctAnswer: "c"},
-    {
-    question: "What HTML attribute references an external JavaScript file?",
-    choiceA: "href",
-    choiceB: "src",
-    choiceC: "class",
-    choiceD: "index",
-    correctAnswer: "b"},
+      question : "What does the acronym DOM stand for?",
+      choiceA : "Digital Object Model",
+      choiceB : "Document Object Model",
+      choiceC : "Display Our Magic",
+      choiceD : "Document Object Model",
+      answer: "Document Object Model"
+    },{
+      question : "Inside which HTML element do we wrap JavaScript?",
+      choiceA : "<scripting>",
+      choiceB : "<css>",
+      choiceC : "<script>",
+      choiceD : "<html>",
+      answer: "<script>"
+    },{
+      question : "The _______ method of an Array object adds and/or removes elements from an array.?",
+      choiceA : "Splice",
+      choiceB : "Shift",
+      choiceC : "String",
+      choiceD : "Sway",
+      answer: "Splice"
+    },{
+      question : "_____  is a box which allows the user to enter input by providing a text box.",
+      choiceA : "Alert",
+      choiceB : "Array",
+      choiceC : "Confirm",
+      choiceD : "Prompt",
+      answer: "Prompt"
+
+    },{
+      question : "What are variables used for in JavaScript Programs?",
+      choiceA : "Storing Numbers, Dates or Other Values",
+      choiceB : "Solving Math Equationgs",
+      choiceC : "Storing Functions",
+      choiceD : "None of the above",
+      answer: "Storing Numbers, Dates or Other Values",
+    },];
+
+startButton.addEventListener("click", function() {
+        console.log("Timer Begin")
+        qIndex = 0
+        startTimer()
+        runQuestions()
+        
+});
+
+    // // Setting Fuction to begin 60 second timer. Need to deduct 5 seconds for every incorrect answer, and timer to reset at 0. Clock currently stops at 0.
+    function startTimer() {
+      var timer = setInterval(function() {
+        count--;
+        if (count == 0){
+          clearInterval (timer)
+        }
+        console.log(count)
+        gameTimer.textContent = count;
+      }, 1000);
+    }
+
+    // Setting function to begin running questions in sequential order, 1-5, using the current-question variable.
+    function runQuestions(){
+      console.log(quizQuestions[qIndex])
+      var thisQuestion = quizQuestions [qIndex]
+      console.log(thisQuestion.question)
+      var questionDisplay = document.createElement("h3")
+      questionDisplay.textContent = thisQuestion.question
+      currentQuestion.appendChild(questionDisplay)
+      var answer1 = document.createElement ("p")
+      answer1.textContent = thisQuestion.choiceA
+      currentQuestion.appendChild(answer1)
+      var answer2 = document.createElement ("p")
+      answer2.textContent = thisQuestion.choiceB
+      currentQuestion.appendChild(answer2)
+      var answer3 = document.createElement ("p")
+      answer3.textContent = thisQuestion.choiceB
+      currentQuestion.appendChild(answer3)
+      var answer4 = document.createElement ("p")
+      answer4.textContent = thisQuestion.choiceB
+      currentQuestion.appendChild(answer4)
+      
+
+
+    
+    }
+    
+    // NEED TO STILL DEVELOP - Was having much difficulty with these criteria sections - 4.17.21 - AS
+    
+    // create radio buttons - button next to each answer to choose answers
+
+    // Local Storage function needed to save user initials and score, post-game.
+  
+    // localStorage.getItem("user", JSON.stringify(playerInitals));
+
+    // let playerInitals = localStorage.getItem(key);
